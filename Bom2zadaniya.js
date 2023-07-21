33dd6f62d3ce9a5c9588eb6cbd764204ed083e38
RenatHuseynzadeh <124487433+RenatHuseynzadeh@users.noreply.github.com>
2023-07-21 10:23:11 +0300

    hw2.0Renat

---
+++ Bom2zadaniya.js
@@ -0,0 +1,41 @@
+// 1
+
+//   function two(arr){
+    //  let obj = {}
+// 
+    //  for(let letter of arr){
+        // obj[letter] = letter.toUpperCase()
+    //  }
+// 
+    //  console.log(obj);
+//   }
+//   
+//   two(['p', 's'])
+
+// 4
+//  let obj1 = {name: 'Max', age: 25, wor: 'school'};
+//  let obj2 = {name: 'Max', age: 25};
+//   
+//  
+//  
+//  if(Object.keys(obj1).length === Object.keys(obj2).length){
+    //  console.log('verno');
+//  } else if(Object.keys(obj1).length !== Object.keys(obj2).length){
+    //  console.log('ne verno');
+//  }
+
+// 5
+//   function two(orders, cost){
+    // let ans = {}
+// 
+// 
+    //    for(let i in orders){
+        //    if(orders[i]>cost){
+            //   ans[i] = orders[i]
+        //    }
+    //    }  
+// 
+    //    console.log(ans);
+//   }
+//   
+//   two({a: 3000, b: 200, c: 1050}, 1000)
